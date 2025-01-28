39a5ee55b68ddb7eb3e95e740757cd3f87bae83f
Tristan Manuel Fischak <t.fischak@shopware.com>
2025-01-28 16:28:14 +0100

    restructure and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { IntroComponent} from './intro/intro.component';
-
 
 export const routes: Routes = [
-  { path: 'intro-component', component: IntroComponent },
+  { path: 'intro', loadComponent: () => import('./view/intro/intro.component').then((c) => c. IntroComponent) },
+  { path: 'home', loadComponent: () => import('./view/home/home.component').then((c) => c. HomeComponent) },
+  { path: 'quiz/:id', loadComponent: () => import('./view/quiz/quiz.component').then((c) => c. QuizComponent) },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', loadComponent: () => import('./core/components/not-found/not-found.component').then((c) => c.NotFoundComponent) },
 ];
