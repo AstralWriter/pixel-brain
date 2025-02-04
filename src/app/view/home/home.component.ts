ed7f3e858696b626a2e9d256d7e5aa5bb3342c58
Tristan Manuel Fischak <t.fischak@shopware.com>
2025-02-04 08:10:22 +0100

    added designs and styling | responsiv

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {GameListComponent} from '../game-list/game-list.component';
 import {IntroComponent} from '../intro/intro.component';
+import {GameComponent} from '../../core/components/game/game.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     GameListComponent,
-    IntroComponent
+    IntroComponent,
+    GameComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
