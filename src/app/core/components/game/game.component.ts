cd05c843df975bc4d3dd6702892e1bd5a54b7729
Tristan Manuel Fischak <t.fischak@shopware.com>
2025-02-04 08:10:22 +0100

    added designs and styling | responsiv

--- src/app/core/components/game/game.component.ts
+++ src/app/core/components/game/game.component.ts
@@ -1,23 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, input } from '@angular/core';
 import {Game} from '../../services/game.model';
-import {GameService} from '../../services/game-service';
+import { RouterLink} from '@angular/router';
 
 @Component({
-  selector: 'app-game',
+  selector: 'game-component',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.less'
 })
 export class GameComponent {
-  games: Game[] = [];
-
-  constructor(private gameService: GameService) {
-  }
-
-  ngOnInit(): void {
-    this.gameService.getGames().subscribe((data) => {
-      this.games = data;
-    });
-  }
+  game = input<Game | undefined>();
 }
--- src/app/view/game-list/game-list.component.ts
+++ src/app/view/game-list/game-list.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { Game } from '../../core/services/game.model';
 import { GameService } from '../../core/services/game-service';
 import {RouterLink} from '@angular/router';
+import {GameComponent} from '../../core/components/game/game.component';
 
 @Component({
   selector: 'game-list-component',
@@ -9,15 +10,14 @@ import {RouterLink} from '@angular/router';
   standalone: true,
   styleUrl: './game-list.component.less',
   imports: [
-    RouterLink
+    RouterLink,
+    GameComponent
   ]
 })
 
 export class GameListComponent implements OnInit {
   games: Game[] = [];
-
-  constructor(private gameService: GameService) {
-  }
+  private gameService = inject(GameService);
 
   ngOnInit(): void {
     this.gameService.getGames().subscribe((data) => {
