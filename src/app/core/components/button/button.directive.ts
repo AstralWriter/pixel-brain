e042959706f8b93349b37b551e6dc5e27a4e81fd
Tristan Manuel Fischak <t.fischak@shopware.com>
2025-02-07 14:20:51 +0100

    design vor options

--- src/app/core/components/button/button.directive.ts
+++ src/app/core/components/button/button.directive.ts
@@ -1,16 +1,14 @@
 import { Directive, Input, computed, input, signal } from '@angular/core';
-import { cva } from 'class-variance-authority';
+import {cva, VariantProps} from 'class-variance-authority';
 import { twMerge } from 'tailwind-merge';
 
-type ButtonVariant = 'default' | 'cta';
-
 const buttonClassVariants = cva(
-  'flex gap-4 rounded-lg justify-center items-center p-4 m-6 absolute left-0 h-16 text-white font-size-[18px] font-semibold',
+  'flex gap-4 rounded-lg justify-center items-center p-4 h-16 text-white font-size-[18px] font-semibold border',
   {
     variants: {
       variant: {
-        default: 'bg-gray-900 b-gray-800 hover:bg-gray-800 hover:border-gray-700 disabled:opacity-50',
-        cta: 'bg-white b-gray-100 text-gray-950 hover:bg-gray-300 hover:b-gray-400',
+        default: 'bg-gray-900 border-gray-800 hover:bg-gray-800 hover:border-gray-700 disabled:opacity-50',
+        cta: 'bg-white border-gray-100 text-gray-950 hover:bg-gray-300 hover:border-gray-400',
       },
     },
     defaultVariants: {
@@ -18,25 +16,22 @@ const buttonClassVariants = cva(
     },
   },
 );
+type ButtonVariants = VariantProps<typeof buttonClassVariants>;
 
 @Directive({
   selector: '[pb-button]',
   standalone: true,
   host: {
     '[class]': '_computedClass()',
-  }
+  },
 })
 export class PbButtonDirective {
   public readonly userClass = input<string>('', { alias: 'class' });
 
   protected _computedClass = computed(() =>
-    twMerge(buttonClassVariants({ variant: this._variant() }), this.userClass()),
+    twMerge(buttonClassVariants({ variant: this.variant() }), this.userClass()),
   );
 
-  private readonly _variant = signal<ButtonVariant>('default');
-  @Input()
-  set variant(variant: ButtonVariant) {
-    this._variant.set(variant);
-  }
+  readonly variant = input<ButtonVariants['variant']>('default');
 }
 
--- src/app/view/quiz/quiz.component.ts
+++ src/app/view/quiz/quiz.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {PbButtonDirective} from '../../core/components/button/button.directive';
+import {OptionComponent} from '../../core/components/option/option.component';
+import {Game} from '../../core/services/game.model';
 
 @Component({
   selector: 'quiz-component',
@@ -8,10 +10,15 @@ import {PbButtonDirective} from '../../core/components/button/button.directive';
   imports: [
     RouterLink,
     PbButtonDirective,
+    OptionComponent,
   ],
+  host: {
+    class: 'h-full w-full flex justify-center',
+  },
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.less'
 })
 export class QuizComponent {
+  game = input<Game | undefined>();
   item = [1,2,3,4];
 }
